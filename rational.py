a644ab0ca7e2ba014b0949b5c1caf7056b6224a9
WATANABE Miki <21716110@jwu.ac.jp>
2019-10-07 16:49:01 +0900

    rational3

--- rational.py
+++ rational.py
@@ -1,11 +1,28 @@
 class Q(object):
     def __init__(self,a,b=1):
-        self.a = a
-        self.b = b
+        gcd = math.gcd(a,b)
+        self.a = a/gcd
+        self.b = b/gcd
     def __repr__(self):
         if self.b == 1:
             return str(self.a)
         return f'{self.a}/{self.b}'
+    def __add__(self,q):
+        a = self.a
+        b = self.b
+        c = q.a
+        d = q.b
+        return Q(a*d+b*c, b*d)
+    def __sub__(self,q):
+        a = self.a
+        b = self.b
+        c = q.a
+        d = q.b
+        return Q(a*d-b*c, b*d)
 
-q = Q(3)
-print(q)
+q1 = Q(1,2)
+q2 = Q(1,3)
+print(q1 + q2)
+print(q1 - q2)
+print(q1 * q2)
+print(q1 / q2)
