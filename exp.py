a611d42737f21e76e5712711b423f4c8b6180c9d
WATANABE Miki <21716110@jwu.ac.jp>
2019-10-28 15:22:26 +0900

    Expr

--- exp.py
+++ exp.py
@@ -1,4 +1,7 @@
-class Val(object):
+class Expr(object):
+    pass
+
+class Val(Expr):
     __slots__ = ['value']
     def __init__(self, value = 0):
         self.value = value
@@ -6,60 +9,29 @@ class Val(object):
         return f'Val({self.value})'
     def eval(self):
         return self.value
+
 v = Val(1)
 print(v)
 assert v.eval() == 1
-class Add(object):
-    __slots__=['left','right']
-    def __init__(self,a,b):
-        self.left = a
+
+assert isinstance(v, Expr) # ==> True
+assert isinstance(v, Val) # ==> True
+assert not isinstance(v, int) 
+
+class Add(Expr):
+    __slots__=['left', 'right']
+    def __init__(self, a, b):
+        self.left = a   # aとb は式
         self.right = b
     def eval(self):
         return self.left.eval() + self.right.eval()
 
-e = Add(Val(1),Val(2))
-print(e.eval())
+e = Add(Val(1), Val(2))  # 1+2
 assert e.eval() == 3
-print(e.eval())
+
+e = Add(1,2)
+assert e.eval() == 3
+
+
 e = Add(Val(1),Add(Val(2),Val(3)))
 assert e.eval() == 6
-print(e.eval())
-
-
-e = Add(Val(1),Add(Val(2),Val(3)))
-class Mul(object):
-    __slots__=['left','right']
-    def __init__(self,a,b):
-        self.left = a
-        self.right = b
-    def eval(self):
-        return self.left.eval() * self.right.eval()
-
-e = Mul(Val(1),Val(2))
-assert e.eval() == 2
-print(e.eval())
-
-
-class Sub(object):
-    __slots__=['left','right']
-    def __init__(self,a,b):
-        self.left = a
-        self.right = b
-    def eval(self):
-        return self.left.eval() - self.right.eval()
-
-e = Sub(Val(1),Val(2))
-assert e.eval() == -1
-print(e.eval()) 
-
-class Div(object):
-    __slots__=['left','right']
-    def __init__(self,a,b):
-        self.left = a
-        self.right = b
-    def eval(self):
-        return self.left.eval() // self.right.eval()
-
-e = Div(Val(7),Val(2))
-assert e.eval() == 3
-print(e.eval()) 
