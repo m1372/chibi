6bc7aec93e25c974f7fad6b2b29d4f500f4acf09
WATANABE Miki <21716110@jwu.ac.jp>
2019-11-25 15:55:44 +0900

    ical2

--- icalc.py
+++ icalc.py
@@ -9,5 +9,17 @@ parser = pegpy.generate(peg)
 
 t = parser('1+2*3')
 print(repr(t))
-t = parser('@2')
+
+def calc(t):
+    if t == 'Int':
+        return int(str(t))
+    if t == 'Add':
+        return calc(t[0]) + calc(t[1])
+    if t == 'Mul':
+        return calc(t[0]) * calc(t[1])
+    print(f'TODO{t.tag}')
+    return 0
+
+t = parser('1+2*3+4*5')
 print(repr(t))
+print(calc(t))
