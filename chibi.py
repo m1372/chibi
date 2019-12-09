a599b5d925b57362ebe83c04e135b8b92edcc4e9
WATANABE Miki <21716110@jwu.ac.jp>
2019-12-09 16:44:17 +0900

    If

--- chibi.py
+++ chibi.py
@@ -52,23 +52,18 @@ class Mod(Binary):
     __slots__ = ['left', 'right']
     def eval(self, env: dict):
         return self.left.eval(env) % self.right.eval(env)
-
-class Eq(Binary): #left == right
+class Eq(Binary):#left=light
     __slots__ = ['left', 'right']
-    def eval(self, env: dict): #cond ? x:y
-        return 1 if self.left.eval(env) == self.right.eval(env) else 0
-
-class Ne(Binary): #left != right
+    def eval(self, env: dict):#cond ? x : y
+        return 1  if self.left.eval(env) == self.right.eval(env) else 0
+class Ne(Binary):#left!=light
     __slots__ = ['left', 'right']
-    def eval(self, env: dict): #cond ? x:y
+    def eval(self, env: dict):#cond ? x : y
         return 1 if self.left.eval(env) != self.right.eval(env) else 0
-
-class Lt(Binary): #left < right
+class Lt(Binary):#left<light
     __slots__ = ['left', 'right']
-    def eval(self, env: dict): #cond ? x:y
+    def eval(self, env: dict):#cond ? x : y
         return 1 if self.left.eval(env) < self.right.eval(env) else 0
-
-
 class Var(Expr):
     __slots__ = ['name']
     def __init__(self, name):
@@ -85,21 +80,56 @@ class Assign(Expr):
     def eval(self, env):
         env[self.name] = self.e.eval(env)
         return env[self.name]
+class Block(Expr):
+    __slots__=['exprs']
+    def __init__(self, exprs):
+        self.exprs=expers
+    def eval(self, env):
+        pass
+class While(Expr):
+    __slots__=['cond', 'body']
+    def __init__(self, cond,body):
+        self.cond=cond
+        self.body=body
+    def eval(self, env):
+        pass
+class If(Expr):
+    __slots__=['cond', 'then','else']
+    def __init__(self, cond, then, else_):
+        self.cond=cond
+        self.then=then
+        self.else_=else_
+    def eval(self, env):
+        yesorno = self.cond.eval(env)
+        if yesorno == 1:
+            return self.then.eval(env)
+        else:
+            return self.else_.eval(env)
 def conv(tree):
     if tree == 'Block':
         return conv(tree[0])
+    if tree == 'If':
+        return If(conv(tree[0]), conv(tree[1]), conv(tree[2]))
+    if tree == 'While':
+        return While(conv(tree[0]), conv(tree[1])
     if tree == 'Val' or tree == 'Int':
         return Val(int(str(tree)))
     if tree == 'Add':
         return Add(conv(tree[0]), conv(tree[1]))
     if tree == 'Sub':
-        return Sub(conv(tree[0]), conv(tree[1]))    
+        return Sub(conv(tree[0]), conv(tree[1]))
     if tree == 'Mul':
         return Mul(conv(tree[0]), conv(tree[1]))
     if tree == 'Div':
         return Div(conv(tree[0]), conv(tree[1]))
     if tree == 'Mod':
         return Mod(conv(tree[0]), conv(tree[1]))
+    if tree == 'Eq':
+        return Eq(conv(tree[0]), conv(tree[1]))
+    if tree == 'Ne':
+        return Ne(conv(tree[0]), conv(tree[1]))
+    if tree == 'Lt':
+        return Lt(conv(tree[0]), conv(tree[1]))
     if tree == 'Var':
         return Var(str(tree))
     if tree == 'LetDecl':
